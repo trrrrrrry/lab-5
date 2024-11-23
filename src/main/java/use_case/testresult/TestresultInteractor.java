45dcbf34a40eec3bf22566464e455c5da9a77971
amiii <ami.wu@mail.utoronto.ca>
2024-11-23 12:54:07 -0500

    Fixed checkstyles in mode selection and test result part refined details and additional tests for mode selection, loggedInview and test result use case

--- src/main/java/use_case/testresult/TestresultInteractor.java
+++ src/main/java/use_case/testresult/TestresultInteractor.java
@@ -1,6 +1,9 @@
 package use_case.testresult;
 
-public class TestresultInteractor implements TestresultInputBoundary{
+/**
+ * This is an interactor for test result use case.
+ */
+public class TestresultInteractor implements TestresultInputBoundary {
     private TestresultDataAccessInterface testresultDataAccessObject;
     private TestresultOutputBoundary testresultPresenter;
 
@@ -12,14 +15,25 @@ public class TestresultInteractor implements TestresultInputBoundary{
 
     @Override
     public void execute(TestresultInputData testresultInputData) {
+        // Validate the input data
+        if (!isValid(testresultInputData)) {
+            testresultPresenter.prepareFailView("Invalid test result provided");
+        }
+
+        // If the input is valid, proceed with prepare success view.
         final TestresultOutputData outputData = new TestresultOutputData(testresultInputData.getCorrectQuestions(),
                 testresultInputData.getTime(), testresultInputData.getIncorrectQuestions());
         testresultPresenter.prepareSuccessView(outputData);
-
     }
 
     @Override
     public void switchToModeselectionView() {
         testresultPresenter.switchToModeselectionView();
+    }
+
+    // Private method to validate the input data
+    private boolean isValid(TestresultInputData testresultInputData) {
+        return testresultInputData.getCorrectQuestions() >= 0
+                | testresultInputData.getTime() > 0 | testresultInputData.getIncorrectQuestions() != null;
     }
 }
