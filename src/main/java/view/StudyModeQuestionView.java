46b38bf392938dded407cf286d6b3e170ba1ba2d
KimberlyFU <kimberlyfu006@gmail.com>
2024-12-01 17:18:07 -0500

    Study/ Test mode question display

--- src/main/java/view/StudyModeQuestionView.java
+++ src/main/java/view/StudyModeQuestionView.java
@@ -3,12 +3,16 @@ package view;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
+import javax.swing.*;
+
+import data_access.DatabaseRetriever;
+import entity.Answer;
+import entity.Question;
 import interface_adapter.logout.LogoutController;
 import interface_adapter.studymodequestion.StudyModeQuestionController;
 import interface_adapter.studymodequestion.StudyModeQuestionViewModel;
@@ -24,26 +28,30 @@ public class StudyModeQuestionView extends JPanel implements ActionListener {
     private StudyModeQuestionController studyModeQuestionController;
 
     private final JLabel username;
-    // TODO: label for question?
+    private final JLabel studymodequestion;
     private final JButton option1;
     private final JButton option2;
     private final JButton option3;
     private final JButton option4;
+    private final JButton nextButton;
 
-    public StudyModeQuestionView(StudyModeQuestionViewModel studyModeQuestionViewModel) {
+    private LinkedList<Question> questions;
+    private Question currentQuestion;
+
+    public StudyModeQuestionView(StudyModeQuestionViewModel studyModeQuestionViewModel) throws SQLException {
         this.studyModeQuestionViewModel = studyModeQuestionViewModel;
+        this.questions = DatabaseRetriever.getQuestionsFromStart(0);
 
         final JLabel title = new JLabel("Study Mode Question");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        // TODO: Add question
-        final JLabel studymodequestion = new JLabel("Question needed to be added ");
+        //        studymodequestion = new JLabel(("Question needed to be added"));
+        studymodequestion = new JLabel("Question needed to be added ");
         studymodequestion.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         final JPanel buttons = new JPanel();
         buttons.setLayout(new GridLayout(2, 3, 10, 10));
 
-        // TODO: Option needed to be added
         option1 = new JButton("Option 1");
         buttons.add(option1);
         option2 = new JButton("Option 2");
@@ -53,63 +61,94 @@ public class StudyModeQuestionView extends JPanel implements ActionListener {
         option4 = new JButton("Option 4");
         buttons.add(option4);
 
+        nextButton = new JButton("Next");
+        // disable nextButton at first
+        nextButton.setEnabled(false);
+        buttons.add(nextButton);
+
         final JLabel usernameInfo = new JLabel("Currently logged in: ");
         usernameInfo.setAlignmentX(Component.CENTER_ALIGNMENT);
         username = new JLabel();
         username.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        loadNextQuestion();
+
         // Take user to 'begin view' page based on their selection
-//        option1.addActionListener(
-//                new ActionListener() {
-//                    public void actionPerformed(ActionEvent evt) {
-//                        if (evt.getSource().equals(option1)) {
-//                            final StudyModeState studyModeState = studyModeQuestionViewModel.getState();
-//                            studyModeState.setModule("Module 1");
-//                            studyModeQuestionController.execute(studyModeState.getModule());
-//                        }
-//                        studyModeQuestionController.switchToStudyModeBeginView();
-//                    }
-//                }
-//        );
-//
-//        option2.addActionListener(
-//                new ActionListener() {
-//                    public void actionPerformed(ActionEvent evt) {
-//                        if (evt.getSource().equals(option2)) {
-//                            final StudyModeState studyModeState = studyModeQuestionViewModel.getState();
-//
-//                            studyModeQuestionController.execute(studyModeState.getModule());
-//                        }
-//                        studyModeQuestionController.switchToStudyModeBeginView();
-//                    }
-//                }
-//        );
-//
-//        option3.addActionListener(
-//                new ActionListener() {
-//                    public void actionPerformed(ActionEvent evt) {
-//                        if (evt.getSource().equals(option3)) {
-//                            final StudyModeState studyModeState = studyModeQuestionViewModel.getState();
-//
-//                            studyModeQuestionController.execute(studyModeState.getModule());
-//                        }
-//                        studyModeQuestionController.switchToStudyModeBeginView();
-//                    }
-//                }
-//        );
-//
-//        option4.addActionListener(
-//                new ActionListener() {
-//                    public void actionPerformed(ActionEvent evt) {
-//                        if (evt.getSource().equals(option4)) {
-//                            final StudyModeState studyModeState = studyModeQuestionViewModel.getState();
-//
-//                            studyModeQuestionController.execute(studyModeState.getModule());
-//                        }
-//                        studyModeQuestionController.switchToStudyModeBeginView();
-//                    }
-//                }
-//        );
+        option1.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (currentQuestion.getAnswers().get(0).isCorrect()) {
+                            option1.setBackground(Color.green);
+
+                        }
+                        else {
+                            option1.setBackground(Color.red);
+                            questions.add(currentQuestion);
+                        }
+                        // Enable "Next" button after an option is selected
+                        nextButton.setEnabled(true);
+                    }
+                }
+        );
+
+        option2.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (currentQuestion.getAnswers().get(1).isCorrect()) {
+                            option2.setBackground(Color.green);
+                        }
+                        else {
+                            option2.setBackground(Color.red);
+                            questions.add(currentQuestion);
+                        }
+                        // Enable "Next" button after an option is selected
+                        nextButton.setEnabled(true);
+                    }
+                }
+        );
+
+        option3.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (currentQuestion.getAnswers().get(2).isCorrect()) {
+                            option3.setBackground(Color.green);
+                        }
+
+                        else {
+                            option3.setBackground(Color.red);
+                            questions.add(currentQuestion);
+                        }
+                        // Enable "Next" button after an option is selected
+                        nextButton.setEnabled(true);
+                    }
+                }
+        );
+
+        option4.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (currentQuestion.getAnswers().get(3).isCorrect()) {
+                            option4.setBackground(Color.green);
+                        }
+                        else {
+                            option4.setBackground(Color.red);
+                            questions.add(currentQuestion);
+                        }
+                        // Enable "Next" button after an option is selected
+                        nextButton.setEnabled(true);
+                    }
+                }
+        );
+
+        nextButton.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        // Disable again until a new option is selected
+                        nextButton.setEnabled(false);
+                        loadNextQuestion();
+                    }
+                }
+        );
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
@@ -119,6 +158,38 @@ public class StudyModeQuestionView extends JPanel implements ActionListener {
 
         this.add(usernameInfo);
         this.add(username);
+    }
+
+    private void loadNextQuestion() {
+        if (!questions.isEmpty()) {
+            currentQuestion = questions.poll();
+            studymodequestion.setText(currentQuestion.getQuestionText());
+
+            final List<Answer> answers = currentQuestion.getAnswers();
+            final JButton[] options = {option1, option2, option3, option4};
+
+            for (int i = 1; i < 5; i++) {
+                if (i < answers.size()) {
+                    options[i].setText(answers.get(i).getAnswerText());
+                    options[i].setEnabled(true);
+                    // Reset button color
+                    options[i].setBackground(null);
+                }
+                else {
+                    options[i].setText("N/A");
+                    options[i].setEnabled(false);
+                }
+            }
+        }
+        else {
+            // End session if no more questions
+            JOptionPane.showMessageDialog(
+                    this,
+                    "You have completed the study mode!",
+                    "Done",
+                    JOptionPane.INFORMATION_MESSAGE
+            );
+        }
     }
 
     public String getViewName() {
